b32f2cfeebf2a3a710f32e9e26a0ba6ae3a9e0e1
Melissa <Melissamcadam24@gmail.com>
2019-07-29 14:41:11 +1000

    added pages and tables to them, to display info

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'profiles/:id/cart' => 'profiles#cart'
+
+  get '/tshirts' => 'pages#tshirts'
+
+  get '/iphonecases' => 'pages#iphonecases'
+
+  get '/stickers' => 'pages#stickers'
+  
+  get '/prints' => 'pages#prints'
+
+  get '/fandoms' => 'pages#fandoms'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
