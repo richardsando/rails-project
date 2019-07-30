8cab0133903db624c8368aafed1f7164781d7657
Richard Sando <richardsando369@gmail.com>
2019-07-30 14:01:18 +1000

    edited seed file

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,6 @@ class Product < ApplicationRecord
   has_many :cart_products, dependent: :destroy
   has_many :carts, :through => :cart_products
   has_many :wishlists, :through => :wishlist_products
-  has_one_attached :uploaded_image
+  has_many_attached :uploaded_image
 
 end
